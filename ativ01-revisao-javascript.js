36ba956ab77b9ce2c63d0a12d225fd390ba568ba
dianety <dianety.slvb@gmail.com>
2021-03-08 02:58:45 +0000

    trabalho de revisao 1

--- ativ01-revisao-javascript.js
+++ ativ01-revisao-javascript.js
@@ -3,15 +3,15 @@
    DADOS PARA O EXERCÍCIO
 
    nome;sigla;area;populacao
-   Sergipe;SE;21915.08;2278308
-   Alagoas;AL;27848.14;3322820
-   Rio Grande do Norte;RN;52811.05;3479010
-   Paraíba;PB;56469.78;3996496
-   Pernambuco;PE;98148.32;9496294
-   Ceará;CE;148920.47;9075649
-   Piauí;PI;251577.74;3264531
-   Maranhão;MA;331937.45;7035055
-   Bahia;BA;564733.18;14812617
+   1 Sergipe;SE;21915.08;2278308
+   2 Alagoas;AL;27848.14;3322820
+   3 Rio Grande do Norte;RN;52811.05;3479010
+   4 Paraíba;PB;56469.78;3996496
+   5 Pernambuco;PE;98148.32;9496294
+   6 Ceará;CE;148920.47;9075649
+   7 Piauí;PI;251577.74;3264531
+   8 Maranhão;MA;331937.45;7035055
+   9 Bahia;BA;564733.18;14812617
 
 */
 
@@ -31,11 +31,86 @@ estadosNe.push(estado1)
       ao vetor estadosNe usando push().
 */
 
+let estado2 = {
+   nome: 'Alagoas',
+   sigla: 'AL',
+   area: 27848.14,
+   populacao: 3322820
+}
+
+estadosNe.push(estado2)
+
+let estado3 = {
+   nome: 'Rio Grande do Norte',
+   sigla: 'RN',
+   area: 52811.05,
+   populacao: 3479010
+}
+
+estadosNe.push(estado3)
+
+let estado4 = {
+   nome: 'Paraíba',
+   sigla: 'PB',
+   area: 56469.78,
+   populacao: 3996496
+}
+
+estadosNe.push(estado4)
+
+let estado5 = {
+   nome: 'Pernambuco',
+   sigla: 'PE',
+   area: 98148.32,
+   populacao: 9496294
+}
+
+estadosNe.push(estado5)
+
+let estado6 = {
+   nome: 'Ceará',
+   sigla: 'CE',
+   area: 148920.47,
+   populacao: 9075649
+}
+
+estadosNe.push(estado6)
+
+let estado7 = {
+   nome: 'Piauí',
+   sigla: 'PI',
+   area: 251577.74,
+   populacao: 3264531
+}
+
+estadosNe.push(estado7)
+
+let estado8 = {
+   nome: 'Maranhão',
+   sigla: 'MA',
+   area: 331937.45,
+   populacao: 7035055
+}
+
+estadosNe.push(estado8)
+
+let estado9 = {
+   nome: 'Bahia',
+   sigla: 'BA',
+   area: 564733.18,
+   populacao: 14812617
+}
+
+estadosNe.push(estado9)
+
 /*
    2) Escreva uma arrow function que receba dois parâmetros, área e população, e
       calcule a densidade demográfica retornando o resultado da divisão da
       segunda pela primeira. 
 */
+const densidadeDemo = (area, pop) => pop / area
+
+console.log(densidadeDemo(564733.18,14812617))
 
 /*
    3) Percorra o vetor estadosNe usando um for tradicional. Para cada estado,
@@ -47,13 +122,33 @@ estadosNe.push(estado1)
 
 */
 
+for (i=0; i<estadosNe.length; i++){
+    let densidade = densidadeDemo(estadosNe[i].area,estadosNe[i].populacao)
+    estadosNe[i]['densidade demográfica'] = densidade
+    delete estadosNe[i].sigla
+}
+
+console.table(estadosNe)
+
 /* 4) Escreva uma arrow function que receba um objeto. Na função, use for..in
       para extrair as propriedades e seus valores e exibi-los com console.log().
 
 */
 
+const recebeObjeto = objeto => {
+    for (let atrib in objeto){
+        console.log(atrib + ': ' + objeto[atrib])
+    }
+}
+
+console.log(recebeObjeto(estadosNe[8]))
+
 /* 5) Percorra o vetor estadosNe usando for..of. Para cada objeto no vetor,
       invoque a função escrita em 4) para exibi-lo.
+*/
+for (let estado of estadosNe){
+    recebeObjeto(estado)
+}
 
 /*
    6)
@@ -67,3 +162,16 @@ estadosNe.push(estado1)
          já existentes, e assim por diante.
 
 */
+let vetor = []
+vetor.push('Sergipe')
+vetor.unshift('Alagoas')
+vetor.splice(1,0,'Rio Grande do Norte')
+vetor.splice(1,0,'Paraíba')
+vetor.splice(2,0,'Pernambuco')
+vetor.splice(1,0,'Ceará')
+vetor.splice(4,0,'Piauí')
+vetor.splice(2,0,'Maranhão')
+vetor.splice(1,0,'Bahia')
+
+
+console.log(vetor)
